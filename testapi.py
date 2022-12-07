c4c1d84029dd859df0523fbfc8a712cab6c6620c
ukit3ro <119313301+ukit3ro@users.noreply.github.com>
2022-12-07 20:01:56 +0500

    Delete testapi.py

--- testapi.py
+++
@@ -1,4 +0,0 @@
-import requests
-data = requests.get('https://openexchangerates.org/api/latest.json?app_id=83cb1073284143dc8d1ff26e9e13a6c3&base=USD&symbols=RUB').json()
-print (data['rates']['RUB'])
-this_token = '5729607125:AAGL9Dw7czWp7X7SDDbHk1Nyn-StfhslMfI'
